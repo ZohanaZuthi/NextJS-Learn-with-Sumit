420063ac9b07f15a44c6c1910f73ae764ce87b93
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-12-31 15:50:45 +0600

    initial commit

--- my-app/app/blog/page.js
+++ my-app/app/blog/page.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-export default function blog() {
-  
+// dummy database
+const blogs = [
+  {
+    id: 1,
+    title: "Blog 1",
+    description: "Blog 1 description",
+  },
+  {
+    id: 2,
+    title: "Blog 2",
+    description: "Blog 2 description",
+  },
+];
 
-  return <div>Blog Page</div>; 
+export default function Blog() {
+  return (
+    <main className="mt-10">
+      <ul>
+        {blogs.map((blog) => (
+          <li className="mb-5" key={blog.id}>
+            {blog.title}
+          </li>
+        ))}
+      </ul>
+
+      <div>Blog Page</div>
+    </main>
+  );
 }
